614bb6f1129fbc3eb5374436a3ff9e60d877c97f
chronoxor <chronoxor@gmail.com>
2015-11-25 19:20:34 +0300

    Update

---
+++ FDK/SoftFX.Extended.Net/Resources/TraceUtils.cs
@@ -0,0 +1,13 @@
+﻿namespace SoftFX.Extended.Resources
+{
+    using System;
+    using System.Diagnostics;
+    static class TraceUtils
+    {
+        public static void WriteLine(String format, params object[] args)
+        {
+            Trace.WriteLine(string.Format(format, args));
+            Console.WriteLine(string.Format(format, args));
+        }
+    }
+}
