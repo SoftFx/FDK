e97374f061335d883576efe18fdf67f2732503b5
Ivan Shynkarenka <chronoxor@gmail.com>
2017-06-05 16:51:32 +0300

    Fix compilation errors

---
+++ FDK/LrpServer.Net/LrpSwapType.cs
@@ -0,0 +1,18 @@
+﻿namespace LrpServer.Net
+{
+    /// <summary>
+    /// Swap type.
+    /// </summary>
+    public enum LrpSwapType
+    {
+        /// <summary>
+        /// Points (by default)
+        /// </summary>
+        Points = 0,
+
+        /// <summary>
+        /// Percentage per year
+        /// </summary>
+        PercentPerYear = 1
+    }
+}
---
+++ FDK/SoftFX.Extended.Net/Enums/SwapType.cs
@@ -0,0 +1,18 @@
+﻿namespace SoftFX.Extended
+{
+    /// <summary>
+    /// Swap type.
+    /// </summary>
+    public enum SwapType
+    {
+        /// <summary>
+        /// Points (by default)
+        /// </summary>
+        Points = 0,
+
+        /// <summary>
+        /// Percentage per year
+        /// </summary>
+        PercentPerYear = 1
+    }
+}
